7aa170a91a7c6db749ea91b8ea7348e6b6e4e0b0
heyManNice <3197871458@qq.com>
2026-08-22 12:11:52 +0800

    perf: cdn字体改为内部安装

--- apps/docs/src/bootloader.tsx
+++ apps/docs/src/bootloader.tsx
@@ -1,4 +1,5 @@
 import "@/bootloader.css";
+import '@lobehub/webfont-harmony-sans-sc/css/index.css';
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router";
